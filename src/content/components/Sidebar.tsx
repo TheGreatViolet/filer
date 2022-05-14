70eccc3772536b3720cc523e592a0378d2b463e9
Violet Hayes <ampc11234@gmail.com>
2022-05-13 21:41:33 -0700

    Cleaned up sidebar

--- src/content/components/Sidebar.tsx
+++ src/content/components/Sidebar.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import { Button, Card, Divider } from '@mantine/core';
+import React, { useEffect, useState } from 'react'
 
 const Sidebar: React.FC = () => {
+  const [userFavorites, setUserFavorites] = useState<string[]>([]);
+
+  useEffect(() => {
+    // Placeholder array for userFavorites
+    setUserFavorites(['Home', 'Desktop', 'Documents']);
+  }, [])
 
   return (
     <>
-      <p >Navbar placeholder</p>
+      <Card sx={{height: '100%'}} p='lg'>
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+          {userFavorites.map((favorite: string, index: number) => {
+            return (
+              <>
+                <Button variant='light' color='dark'>{favorite}</Button>
+                {index !== userFavorites.length - 1 ? <Divider /> : null}
+              </>
+            )
+          })}
+        </div>
+      </Card>
     </>
   );
 }
