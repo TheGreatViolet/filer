99f98790f1b16db5dafddb27110de392e2fd2eb2
Violet Hayes <ampc11234@gmail.com>
2022-05-21 13:10:08 -0700

    Created functions for indexing folders

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,7 +5,7 @@
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![get_favorite_folders])
+    .invoke_handler(tauri::generate_handler![get_favorite_folders, index_folder])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
@@ -45,5 +45,22 @@ async fn get_favorite_folders() -> Vec<String> {
     }
   } else {
     return [].to_vec();
+  }
+}
+
+#[tauri::command]
+async fn index_folder(folder_name: String, fav: bool) -> () {
+  let prisma_client: Result<PrismaClient, NewClientError> = client::new_client().await;
+
+  if let Ok(client) = prisma_client {
+    let _folder = client
+      .folder()
+      .create(
+        folder::name::set(folder_name),
+        folder::favorite::set(fav),
+        vec![],
+      )
+      .exec()
+      .await;
   }
 }
--- src/functions/data.ts
+++ src/functions/data.ts
@@ -3,3 +3,7 @@ import { invoke } from "@tauri-apps/api/tauri";
 export const getFavFolders = async (): Promise<string[]> => {
   return invoke("get_favorite_folders");
 }
+
+export async function indexFolder(name: string, fav: boolean = false): Promise<void> {
+  return invoke("index_folder", { folderName: name, fav: fav });
+}
